66191e072c87a75068dc0311d3f5c23f977e442d
GodzK <64467457+GodzK@users.noreply.github.com>
2024-01-14 21:35:59 +0700

    algrorithms

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("pk is here")
+const os = require('os');
+const path = require('path')
+const {add,sub,mul} = require('./math')//โยงทีเดียว เเล้วไปใช้ได้เลย
+
+
+
+
+
+console.log(mul(2,4))
+
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
+// console.log(global)
