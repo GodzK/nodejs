aa5ade5e2930d29c656cc013aa7b9f3ae4e38591
GodzK <64467457+GodzK@users.noreply.github.com>
2024-01-16 19:42:43 +0700

    div

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const os = require('os');
 const path = require('path')
-const {add,sub,mul} = require('./math')//โยงทีเดียว เเล้วไปใช้ได้เลย
+const {add,sub,mul,div} = require('./math')//โยงทีเดียว เเล้วไปใช้ได้เลย
 
 
 
 
 
-console.log(mul(2,4))
+console.log(div(1,4))
 
 // console.log(path.dirname(__filename))
 // console.log(path.basename(__filename))
