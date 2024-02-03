5f36757e468fd94ec8bdd4358c6594c664266632
Phakaphol <godzk25@gmail.com>
2024-02-03 08:45:35 +0700

    route handler

--- EP3K/index.js
+++ EP3K/index.js
@@ -2,9 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
-// any get request come in
 
-app.get('/', (req, res) => {
-    res.send("Hello world!");
-})
+app.get('/index(.html)?', (req, res) => {
+    // ข้างบนคือการทำให้urlของเว็บไม่มีชื่อสกุลfile
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});     
+
+app.get('/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+// route handler 
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
